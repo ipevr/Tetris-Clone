b7235c0cbc47ccb72c332cac1e2365130161d388
Ingo Peters <ingo.peters2@gmx.de>
2019-02-01 22:56:40 +0100

    - Tetris Clone

---
+++ Assets/Scripts/Core/Board.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] int height = 30;
+    [SerializeField] int width = 10;
+
+    Transform[,] grid;
+
+    void Awake() {
+        grid = new Transform[width, height];
+    }
+
+}
---
+++ Assets/Scripts/Core/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour {
+    [SerializeField] bool canRotate = true;
+
+    private void Start() {
+
+    }
+
+    void Move(Vector3 moveDirection) {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft() {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight() {
+        Move(new Vector3(1, 0, 0));
+    }
+
+    public void MoveUp() {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void MoveDown() {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void RotateRight() {
+        if (canRotate) {
+            transform.Rotate(new Vector3(0, 0, -90));
+        }
+    }
+
+    public void RotateLeft() {
+        if (canRotate) {
+            transform.Rotate(new Vector3(0, 0, 90));
+        }
+    }
+}
