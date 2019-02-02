6c3a2a5186afc114523631ffb37b1708180b15c0
Ingo Peters <ingo.peters2@gmx.de>
2019-02-02 16:47:02 +0100

    - added a gameManager - added a spawner class that spawns shapes

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] Board gameBoard;
+    [SerializeField] Spawner spawner;
+
+    Shape spawnedShape = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedShape = spawner.SpawnShape();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Managers/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    [SerializeField] Shape[] shapes;
+    [SerializeField] Transform spawnPoint;
+
+    Shape GetRandomShape() {
+        return shapes[Random.Range(0, shapes.Length)];
+    }
+
+    public Shape SpawnShape() {
+        return Instantiate(GetRandomShape(), Vector3Int.RoundToInt(spawnPoint.position), Quaternion.identity);
+    }
+
+}
