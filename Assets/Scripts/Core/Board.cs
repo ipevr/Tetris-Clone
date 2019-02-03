d93a5b33f52324668f89125db033065085556d57
Ingo Peters <ingo.peters2@gmx.de>
2019-02-03 14:05:20 +0100

    - keyboard input to rotate, translate and drop down shapes -

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -13,4 +13,29 @@ public class Board : MonoBehaviour
         grid = new Transform[width, height];
     }
 
+    bool IsWithinBoard(int x, int y) {
+        return ((x >= 0 && x < width) && (y >= 0 && y < height));
+    }
+
+    bool IsInFreeGrid(int x, int y) {
+        return grid[x, y] == null;
+    }
+
+    public bool HasShapeValidPosition(Shape shape) {
+        foreach(Transform child in shape.transform) {
+            Vector2Int pos = Vector2Int.RoundToInt(child.position);
+            if (!IsWithinBoard(pos.x, pos.y) || !IsInFreeGrid(pos.x, pos.y)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void StoreShapeInGrid(Shape shape) {
+        foreach (Transform child in shape.transform) {
+            Vector2Int pos = Vector2Int.RoundToInt(child.position);
+            grid[pos.x, pos.y] = child;
+        }
+    }
+
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,23 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    [SerializeField] Board gameBoard;
-    [SerializeField] Spawner spawner;
+    [SerializeField] Board board = null;
+    [SerializeField] Spawner spawner = null;
+    [SerializeField] float dropDownInterval = .5f;
 
-    Shape spawnedShape = null;
+    Shape activeShape = null;
+    float timeToDrop = 0f;
+    float originalDropDownInterval = 0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        spawnedShape = spawner.SpawnShape();
+    void Start() {
+        originalDropDownInterval = dropDownInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!activeShape) {
+            activeShape = spawner.SpawnShape();
+        } else {
+            DropDownOverTime();
+            CheckForInput();
+        }
+    }
+
+    void CheckForInput() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            activeShape.RotateLeft();
+            if (!board.HasShapeValidPosition(activeShape)) {
+                activeShape.RotateRight();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            activeShape.MoveLeft();
+            if (!board.HasShapeValidPosition(activeShape)) {
+                activeShape.MoveRight();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            activeShape.MoveRight();
+            if (!board.HasShapeValidPosition(activeShape)) {
+                activeShape.MoveLeft();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            dropDownInterval = 0;
+            timeToDrop = Time.time;
+            DropDownOverTime();
+        }
+    }
+
+    void DropDownOverTime() {
+        if (Time.time > timeToDrop) {
+            activeShape.MoveDown();
+            timeToDrop = Time.time + dropDownInterval;
+            SpawnNewShapeWhenPlaced();
+        }
+    }
+
+    void SpawnNewShapeWhenPlaced() {
+        if (!board.HasShapeValidPosition(activeShape)) {
+            activeShape.MoveUp();
+            board.StoreShapeInGrid(activeShape);
+            activeShape = spawner.SpawnShape();
+            dropDownInterval = originalDropDownInterval;
+        }
     }
 }
